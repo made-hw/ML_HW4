dcd469a0b908a7fab211798a9117cb951da47ebb
aleks <alex.iv.vad@gmail.com>
2021-01-07 09:13:14 +0300

    update IP

--- demo/app.py
+++ demo/app.py
@@ -1,5 +1,5 @@
-import os
 import pathlib
+import pickle
 
 import dash
 import dash_table
@@ -7,17 +7,13 @@ from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
-import dash_daq as daq
 
 import plotly.graph_objs as go
 
 import pandas as pd
 import numpy as np
 
-import matplotlib.pyplot as plt
-
 BASE_PATH = pathlib.Path(__file__).parent.resolve()
-DATA_PATH = BASE_PATH.joinpath("aux_data").resolve()
 
 external_stylesheets = [dbc.themes.BOOTSTRAP, ]
 app = dash.Dash(
@@ -28,39 +24,50 @@ app = dash.Dash(
     ], )
 server = app.server
 
-data_filename = DATA_PATH.joinpath('.csv')
+
+# ######################################
+# Данные
+# ######################################
 
 class Binning:
-    def __init__(self, gaps, woes, counts, name=None):
+    def __init__(self, gaps, woes, iv, shares, counts, gini, gaps_counts_shares, gaps_avg=[], hhi=-1, r2=0, name=None):
         self._name = name
         self._gaps = gaps
         self._woes = woes
+        self._iv = iv
+        self._shares = shares
         self._counts = counts
+        self._gini = gini
+        self._r2 = r2
+        self._hhi = hhi
+        self._gaps_counts_shares = gaps_counts_shares
+        self._gaps_avg = gaps_avg
 
-factors_list = [
-    {'label': 'Возраст организации (ОГРН)', 'value': 'ogrn_age_woe'},
-    {'label': 'Возраст организации (юр. адрес)', 'value': 'adr_actual_age_woe'},
-    {'label': 'Капитал', 'value': 'ul_capital_sum_woe'},
-    {'label': 'Кол-во акционеров', 'value': 'ul_founders_cnt_woe'}]
 
-h_df = pd.DataFrame(
-    data=[
-        ['Возраст оргранизации (ОГРН)', 0.34],
-        ['Возраст организации (юр. адрес)', 0.12],
-        ['Капитал', 0.08],
-        ['Кол-во акционеров', 0.14]],
+with open(BASE_PATH.joinpath('bins_ip.pkl'), 'rb') as f:
+    bins_ip = pickle.load(f)
+
+ip_factors_list = [
+    {'label': 'Возраст организации (ОГРН)', 'value': 'ogrn_age', 'gini': 0.345, 'default': 76},
+    {'label': 'Возраст организации (юр. адрес)', 'value': 'adr_actual_age', 'gini': 0.125, 'default': 27},
+    {'label': 'Капитал', 'value': 'ul_capital_sum', 'gini': 0.080, 'default': 10000},
+    {'label': 'Кол-во акционеров', 'value': 'ul_founders_cnt', 'gini': 0.144, 'default': 2}]
+
+ip_df = pd.DataFrame(
+    data=[[x['label'], x['gini']] for x in ip_factors_list],
     columns=['factor_name', 'gini_score'])
 
-bining = Binning(
-    [
-        [-100000000000000, 28.5],
-        [28.5, 50.5],
-        [50.5, 81.5],
-        [81.5, 131.5],
-        [131.5, 100000000000000]],
-    [-0.7440409894342532, -0.42755947853297643, 0.104732426381871, 0.573439441602871, 0.8749172074201356],
-    [[1090, 261], [1021, 178], [1164, 119], [1471, 94], [1673, 79]],
-    'sdfsg')
+ip_forms_el = []
+for i in ip_factors_list:
+    ip_forms_el.append(
+        dbc.FormGroup(
+            [
+                dbc.Label(i['label']),
+                dbc.Col(dbc.Input(type='number', id=i['value'], placeholder=i['default'], value=i['default'])),
+            ], row=True,
+        )
+    )
+ip_form = dbc.Form(ip_forms_el)
 
 
 def data_bars(df, column):
@@ -110,17 +117,17 @@ def one_bin_barchart(bining, size=5):
     # линия - доля плохих в бакете
     bins_dr = [b[1] / (b[0] + b[1]) for b in bining._counts if len(b) == 2]
     trace1 = go.Bar(
-            x=total_bins,
-            y=bins_counts,
-            yaxis='y1',
-            name="Кол-во набл.",
-        )
+        x=total_bins,
+        y=bins_counts,
+        yaxis='y1',
+        name="Кол-во набл.",
+    )
     trace2 = go.Scatter(
-            x=total_bins,
-            y=bins_dr,
-            yaxis='y2',
-            name="Доля дефолтов",
-        )
+        x=total_bins,
+        y=bins_dr,
+        yaxis='y2',
+        name="Доля дефолтов",
+    )
 
     data = [trace1, trace2]
     layout = go.Layout(
@@ -141,21 +148,9 @@ def one_bin_barchart(bining, size=5):
 
     return fig
 
-forms_el = []
-for i in factors_list:
-    forms_el.append(
-        dbc.FormGroup(
-            [
-                dbc.Label(i['label']),
-                dbc.Col(dbc.Input(type='number', id=i['value'], placeholder="Введите число")),
-            ], row=True,
-        )
-    )
-
-form = dbc.Form(forms_el)
 
 # ######################################
-# Dash specific part
+# Структура
 # ######################################
 
 # The app layout
@@ -177,9 +172,9 @@ app.layout = dbc.Container([
                    'результатов моделирования.'),
         ], ),
         dbc.Col([
-            html.A('исходный код данного', href=""),
+            html.A('исходный код данного', href="https://github.com/made-hw/ML_HW4/blob/master/Solution_and_presenattion.ipynb"),
             html.Br(),
-            html.A('код разработки модели', href=""),
+            html.A('код разработки модели', href="https://github.com/made-hw/ML_HW4/tree/master/demo"),
         ], width=3),
     ]),
     dbc.Row([
@@ -187,7 +182,7 @@ app.layout = dbc.Container([
             html.Div(
                 dbc.Row([
                     dbc.Col(html.Label("Gini")),
-                    dbc.Col(html.B('42%', id="score_text", style={'fontSize': 40, 'color': '#1EAEDB', 'opacity': 0.5})),
+                    dbc.Col(html.B('40%', id="score_text", style={'fontSize': 40, 'color': '#1EAEDB', 'opacity': 0.5})),
                 ]),
                 id="score",
                 className="pretty_container",
@@ -197,7 +192,7 @@ app.layout = dbc.Container([
             html.Div(
                 dbc.Row([
                     dbc.Col(html.Label("Доля дефолтов")),
-                    dbc.Col(html.B('6%', id="dr_text", style={'fontSize': 40, 'color': '#1EAEDB', 'opacity': 0.5})),
+                    dbc.Col(html.B('10.2%', id="dr_text", style={'fontSize': 40, 'color': '#1EAEDB', 'opacity': 0.5})),
                 ]),
                 id="dr",
                 className="pretty_container",
@@ -206,7 +201,7 @@ app.layout = dbc.Container([
         dbc.Col([
             html.Div(className="pretty_container", children=[
                 html.Label('Тип клиентов'),
-                dcc.Dropdown(id='days',
+                dcc.Dropdown(id='cl_type',
                              placeholder='Выберите клиентов',
                              options=[{'label': 'Без фин. отчётн.', 'value': 0},
                                       {'label': 'С фин. отчётностью', 'value': 1}, ],
@@ -235,8 +230,8 @@ app.layout = dbc.Container([
                                     {'name': 'Gini', 'id': 'gini_score'},
                                 ],
 
-                                data=h_df.to_dict('records'),
-                                style_data_conditional=data_bars(h_df, 'gini_score'),
+                                data=ip_df.to_dict('records'),
+                                style_data_conditional=data_bars(ip_df, 'gini_score'),
                                 style_as_list_view=True,
                             )
                         ])
@@ -253,21 +248,75 @@ app.layout = dbc.Container([
                         dbc.Col([
                             dcc.Dropdown(
                                 id='factors',
-                                placeholder='Выберите клиентов',
-                                options=factors_list,
-                                value=factors_list[0]['value'],
+                                placeholder='Выберите фактор',
+                                options=[{'label': x['label'], 'value': x['value']} for x in ip_factors_list],
+                                value=bins_ip[0]._name,
                                 multi=False),
-                            dcc.Graph(id='matplotlib-graph', figure=one_bin_barchart(bining, size=5))
+                            dcc.Graph(id='matplotlib-graph', figure=one_bin_barchart(bins_ip[0], size=5))
                         ], width=9)
                     ]),
                 ]),
                 dcc.Tab(label='Скоринг', children=[
-                    form, dbc.Alert(f"Вероятность дефолта {7}%", color="info"),
+                    ip_form, dbc.Alert(f"Вероятность дефолта {7}%", color="info", id='alert'),
                 ]),
             ]),
         ], width=12),
     ]),
 ])
 
+
+# ######################################
+#  Обновления
+# ######################################
+
+@app.callback(
+    [
+        Output("score_text", "children"),
+        Output("dr_text", "children"),
+        Output("table", "data"),
+        Output("table", "style_data_conditional"),
+        Output("factors", "options"),
+        Output("factors", "value"),
+    ],
+    Input("cl_type", "value"),
+)
+def update_text(data):
+    if data == 0:
+        return '40.0%', '10.2', ip_df.to_dict('records'), data_bars(ip_df, 'gini_score'), [
+            {'label': x['label'], 'value': x['value']} for x in ip_factors_list], ip_factors_list[0]['value']
+    else:
+        return '33', '44', ip_df.to_dict('records'), data_bars(ip_df, 'gini_score'), [
+            {'label': x['label'], 'value': x['value']} for x in ip_factors_list], ip_factors_list[0]['value']
+
+
+@app.callback(
+    Output("matplotlib-graph", "figure"),
+    [Input("cl_type", "value"), Input("factors", "value"), ]
+)
+def update_graph(data1, data2,):
+    if data1 == 0:
+        return one_bin_barchart([x for x in bins_ip if x._name == data2][0], size=5)
+    else:
+        return one_bin_barchart([x for x in bins_ip if x._name == data2][0], size=5)
+
+
+@app.callback(
+    Output("alert", "children"),
+    [
+        Input("cl_type", "value"), *[Input(x['value'], 'value') for x in ip_factors_list]
+    ]
+)
+def update_calc(data1, *data2):
+    if data1 == 0:
+        data2 = [0 if x is None else x for x in data2]
+        take_bin = lambda name: [x for x in bins_ip if x._name == name][0]
+        take_woe = lambda v, name: take_bin(name)._woes[[(i, x) for (i, x) in enumerate(take_bin(name)._gaps) if x[0] <= v < x[1]][0][0]]
+        lr = np.sum([x[0] * x[1] for x in zip([take_woe(x[1], x[0]['value']) for x in zip(ip_factors_list, data2)], [-0.94890533, -0.65224689, -0.86978949, -0.96749802])])
+        lr += -2.18364137
+        return f'Вероятность дефолта {round(np.exp(lr) / (1 + np.exp(lr)) * 100, 2)} %'
+    else:
+        return one_bin_barchart([x for x in bins_ip if x._name == data2][0], size=5)
+
+
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=False)
